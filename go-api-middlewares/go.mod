aade6bc0fce4850329139153e9306f8b201b1cbc
0oAVIRALo0 <aviral21029@iiitd.ac.in>
2025-06-14 19:41:39 +0530

    implemented middlewares

---
+++ go-api-middlewares/go.mod
@@ -0,0 +1,3 @@
+module github.com/0oAVIRALo0/go-rest-api
+
+go 1.24.2
---
+++ go-api-middlewares/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "log"
+
+func main() {
+	server := NewAPIServer(":8080")
+	err := server.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
